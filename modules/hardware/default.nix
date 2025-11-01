b8be19f0a82b288648bd9223b6cfcf1ae4926aa7
user@example.com <user>
2025-11-01 13:33:22 +0000

    a

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -31,8 +31,8 @@ in {
           "flakes" 
         ];
         substituters = [
-          "https://cache.main.com"
           "https://nix-community.cachix.org"
+          "https://cache.nixos.org"
         ];
         trusted-public-keys = [ 
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
